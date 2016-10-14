51bdc6a0f2082ff377d570c57f0911efc60ca837
区泳传 <ouyongchuan@quyongchuandemac.local>
2016-10-14 07:27:10 -0700

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by 区泳传 on 16/10/14.
+//  Copyright © 2016年 区泳传. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by 区泳传 on 16/10/14.
+//  Copyright © 2016年 区泳传. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
